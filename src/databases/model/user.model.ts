f8c9cdf026423a8b3e26e540034068c787d076a4
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-16 14:26:27 +0530

    create user schema

---
+++ src/databases/model/user.model.ts
@@ -0,0 +1,12 @@
+import {Document} from 'mongoose';
+import {IProduct} from './product.model';
+import {IAddress} from './address.model';
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    cart: IProduct[],
+    wishlist: IProduct[],
+    addresses: IAddress[],
+}
---
+++ src/databases/schema/user.schema.ts
@@ -0,0 +1,45 @@
+import mongoose, {Schema, model} from 'mongoose';
+import {IUser} from '../model/user.model';
+import Product from '../../databases/schema/product.schema';
+import Address from '../../databases/schema/address.schema';
+
+const schema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        cart: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: Product
+            }
+        ],
+        wishlist: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: Product
+            }
+        ],
+        addresses: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: Address
+            }
+        ]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default model<IUser>('User', schema);
